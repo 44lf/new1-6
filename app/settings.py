4bb57ba262d16cbe6b0d6881ee1cdf89d8f0c48f
豆豆又包包 <1619728742@qq.com>
2026-01-07 14:37:26 +0800

    12

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ TORTOISE_ORM = {
 
 
 # --- MinIO 配置 ---
-MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "127.0.0.1:9000")
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "127.0.0.1:9100")
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
 MINIO_BUCKET_NAME = "resumes"  # 你的桶名字
